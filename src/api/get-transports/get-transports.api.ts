470c060fdc66d27af55e1bfc5ce913f279472a61
Sasha Pimenov <sasha.pimenov.2002@inbox.ru>
2024-01-02 17:31:09 +0500

    ins react-select and orders-create page

---
+++ src/api/get-transports/get-transports.api.ts
@@ -0,0 +1,21 @@
+export interface GetTransportsResponseDto {
+  transports: {
+    name: string;
+    plateNumber: string;
+    color: string;
+  }[];
+}
+
+export const getTransports = async (accessToken: string) => {
+  const res = await fetch(
+    "https://urfu-nvk.ru/profile/api/v1/data/transports",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${accessToken}`,
+      },
+    }
+  );
+  const responseDto: GetTransportsResponseDto = await res.json();
+  return responseDto;
+};
