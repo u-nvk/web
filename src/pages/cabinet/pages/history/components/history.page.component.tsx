19654dcba4df5a62b7d258a658cdc40b82f52992
FireFace1337 <dimakorobitcyn@yandex.ru>
2024-01-02 22:59:36 +0500

    Create history page

---
+++ src/pages/cabinet/pages/history/components/history.page.component.tsx
@@ -0,0 +1,55 @@
+import styles from "../styles/history.component.module.css";
+
+type RouteProps = {
+  route: string;
+  departureTimeShort: string;
+  price: string;
+  emptySeat: number;
+  isUserJoin: boolean;
+  isUserDriver: boolean;
+};
+export const HistoryPageComponent = ({
+  route,
+  departureTimeShort,
+  price,
+  isUserDriver,
+  isUserJoin,
+}: RouteProps) => {
+  return (
+    <div className={`${styles.container} ${isUserJoin ? styles.borderGreen : ''} ${isUserDriver ? styles.borderYellow : ''}`}>
+      <div className={styles.directionContainer}>
+        <p className={`${styles.directionText} mediumText`}>{route}</p>
+      </div>
+      <div className={styles.infoContainer}>
+        <div
+          className={`${styles.orderInfoElementDiv} ${styles.orderInfoElementDivFirst}`}
+        >
+          <div className={styles.orderInfoElementDivText}>
+            <p className={`${styles.orderInfoElementDescription} lightText`}>
+              отправление
+            </p>
+            <p className={`${styles.orderInfoElementValue} mediumText`}>
+              {departureTimeShort}
+            </p>
+          </div>
+        </div>
+        <div
+          className={`${styles.orderInfoElementDiv} ${styles.orderInfoElementDivSecond}`}
+        >
+          <div className={styles.orderInfoElementDivText}>
+            <p className={`${styles.orderInfoElementDescription} lightText`}>
+              стоимость
+            </p>
+            <p className={`${styles.orderInfoElementValue} mediumText`}>
+              {price}₽
+            </p>
+          </div>
+        </div>
+        <div
+          className={`${styles.orderInfoElementDiv} ${styles.orderInfoElementDivThird}`}
+        >
+        </div>
+      </div>
+    </div>
+  );
+};
