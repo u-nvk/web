ecb0da2a31dc87639ddddaf912ec570c28d31e12
Dmitry <dimakorobitcyn@yandex.ru>
2024-01-14 23:45:15 +0500

    Add new places

--- src/pages/cabinet/pages/orders/pages/create/orders-create.page.tsx
+++ src/pages/cabinet/pages/orders/pages/create/orders-create.page.tsx
@@ -58,17 +58,19 @@ const defaultDirections: CustomChip[] = [
 
 const defaultPlaces: IPlace[] = [
   { value: "MUB", label: "ГУК" },
-  { value: "D11", label: "Общежитие №11" },
   { value: "G8M", label: "Гринвич со стороны 8 марта" },
   { value: "UGI", label: "УГИ" },
+  { value: "BT", label: "Метро Ботаническая" },
+  { value: "RTF", label: "ИРИТ-РТФ" },
+  { value: "D11", label: "Общежитие №11" },
   { value: "RV", label: "ЖД Вокзал" },
   { value: "AR", label: "Екатеринбург-Арена" },
+  { value: "INEU", label: "Корпус ИНЭУ, Гоголя 25"},
+  { value: "IENIM", label: "Корпус ИЕНиМ, Куйбышева 48"},
   { value: "VZ", label: "ВИЗ" },
-  { value: "BT", label: "Метро Ботаническая" },
   { value: "UM", label: "Уралмаш" },
   { value: "YV", label: "Южный автовокзал" },
-  // { value: "4", label: "Вторчермет" },
-  // { value: "5", label: "Ботаника" },
+  { value: "VTOR", label: "Вторчермет"}
 ];
 
 const customStyles = {
